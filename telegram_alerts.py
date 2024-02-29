7b4cf6da55751ea07d4f4b9eccd0dfc69613bc64
Sokinjo <160136911+SokinjoNS@users.noreply.github.com>
2024-02-29 23:16:39 +0100

    Update telegram_alerts.py

--- telegram_alerts.py
+++ telegram_alerts.py
@@ -1,12 +1,16 @@
 import requests
 import json
+from formatting_btk import format_number  # Import the formatting function
 
 def load_telegram_credentials():
     with open('credentials_telegram.json') as f:
         credentials = json.load(f)
     return credentials
 
-def send_telegram_message(message):
+def send_telegram_message(message, volume=None):
+    if volume is not None:
+        formatted_volume = format_number(volume)  # Format the volume
+        message += f"\nVolume: {formatted_volume}"  # Append formatted volume to the message
     credentials = load_telegram_credentials()
     bot_token = credentials['Telegram_bot_token']
     chat_id = credentials['Telegram_chat_id']
