fd033760421064e8a6a1d09a924a4da96c9b6c4e
Sokinjo <160136911+SokinjoNS@users.noreply.github.com>
2024-03-02 22:34:19 +0100

    Update telegram_alerts.py

--- telegram_alerts.py
+++ telegram_alerts.py
@@ -1,19 +1,26 @@
 import requests
 import json
-from formatting_btk import format_number  # Import the formatting function
+from formatting_btk import format_number
+from alert_levels_tg import get_volume_alert_details
 
 def load_telegram_credentials():
     with open('credentials_telegram.json') as f:
         credentials = json.load(f)
     return credentials
 
-def send_telegram_message(message, volume=None):
-    if volume is not None:
-        formatted_volume = format_number(volume)  # Format the volume
-        message += f"\nVolume: {formatted_volume}"  # Append formatted volume to the message
+def send_telegram_message(alert_details):
     credentials = load_telegram_credentials()
     bot_token = credentials['Telegram_bot_token']
     chat_id = credentials['Telegram_chat_id']
+    
+    # Formulate the message based on alert_details
+    message = f"*{alert_details['exchange']} Alert*\n"
+    message += f"Symbol: {alert_details['symbol']}\n"
+    message += f"Current Volume: {format_number(alert_details['curr_volume'])}\n"
+    message += f"Mean Volume (24h): {format_number(alert_details['prev_volume_mean'])}\n"
+    message += f"Alert Level: {alert_details['level']}\n"
+    message += f"[View Chart]({alert_details['chart_url']})"
+
     send_text = f'https://api.telegram.org/bot{bot_token}/sendMessage?chat_id={chat_id}&parse_mode=Markdown&text={message}&disable_web_page_preview=True'
     response = requests.get(send_text)
     return response.json()
